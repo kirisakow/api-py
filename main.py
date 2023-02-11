9bfa78101e53c24b93bf18d0ce99b2605cda5c1c
Kiril Isakov <kirix.isakov@gmail.com>
2023-02-11 13:49:19 +0100

    Stricter regex pattern for HTML entity. Also, strip '\n' if any

--- main.py
+++ main.py
@@ -35,19 +35,21 @@ app = FastAPI(
 # http://localhost:3000/unescape_url/https://uk.wikipedia.org/wiki/%D0%A3%D0%BA%D1%80%D0%B0%D1%97%D0%BD%D0%B0
 # http://localhost:3000/unescape_url/https://uk.wikipedia.org/wiki/%u0423%u043A%u0440%u0430%u0457%u043D%u0430
 @app.get("/unescape_url/{url_to_unescape:path}")
-async def unescape_url(url_to_unescape: str = Query(alias='url', title='Tiiiiitle', description='Descriiiiption descriiiiption descriiiiption')):
-    if url_to_unescape:
-        regex_patterns = {'html_entity': r'&#?\w+;',
-                          'escaped': r'%[uU]([0-9A-Fa-f]{4})', 'percent_encoded': r'%([0-9A-Fa-f]{2})'}
-        how_many_passes = 3
-        for _ in range(how_many_passes):
-            if re.search(regex_patterns['html_entity'], url_to_unescape):
-                url_to_unescape = html.unescape(url_to_unescape)
-            if re.search(regex_patterns['escaped'], url_to_unescape):
-                url_to_unescape = re.sub(regex_patterns['escaped'], lambda m: chr(
-                    int(m.group(1), 16)), url_to_unescape)
-            if re.search(regex_patterns['percent_encoded'], url_to_unescape):
-                url_to_unescape = urllib.parse.unquote(url_to_unescape)
+async def unescape_url(url_to_unescape: str = Query(alias='url', title='Tiiiiitle', description='Descriiiiption descriiiiption descriiiiption')) -> str:
+    if not url_to_unescape:
+        raise ValueError('')
+    url_to_unescape = url_to_unescape.strip('\n')
+    regex_patterns = {'html_entity': r'[&]([a-z]+)|([#][0-9]+)[;]',
+                      'escaped': r'%[uU]([0-9A-Fa-f]{4})', 'percent_encoded': r'%([0-9A-Fa-f]{2})'}
+    how_many_passes = 3
+    for _ in range(how_many_passes):
+        if re.search(regex_patterns['html_entity'], url_to_unescape):
+            url_to_unescape = html.unescape(url_to_unescape)
+        if re.search(regex_patterns['escaped'], url_to_unescape):
+            url_to_unescape = re.sub(regex_patterns['escaped'], lambda m: chr(
+                int(m.group(1), 16)), url_to_unescape)
+        if re.search(regex_patterns['percent_encoded'], url_to_unescape):
+            url_to_unescape = urllib.parse.unquote(url_to_unescape)
     return url_to_unescape
 
 
