3e9bceb253665be45b21956885b6a9736ee2a0dc
Kiril Isakov <kirix.isakov@gmail.com>
2023-02-11 18:26:29 +0100

    Replace in-place processing by a call to url_unescape func from url_tools repo. See commit: https://github.com/kirisakow/url_tools/commit/9dcb5e328e8f4168ac182220cecb1cf93adce083

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
-import html
-import re
-import os
-import urllib.parse
 from fastapi import FastAPI, Query
+import html
+import os
+import re
+import subprocess
+import sys
+import urllib.parse
 from whatever_disentangler import whatever_disentangler as wd
 
 
 app = FastAPI(
     description='A few tools I created for myself and made available as an HTTP API', )
 
+
 # http://localhost:3000/urlunescape?url=https://uk.wikipedia.org/wiki/%D0%A3%D0%BA%D1%80%D0%B0%D1%97%D0%BD%D0%B0
 # http://localhost:3000/urlunescape?url=https://uk.wikipedia.org/wiki/%u0423%u043A%u0440%u0430%u0457%u043D%u0430
-
-
 @app.get("/urlunescape")
 # http://localhost:3000/urlunescape/https://uk.wikipedia.org/wiki/%D0%A3%D0%BA%D1%80%D0%B0%D1%97%D0%BD%D0%B0
 # http://localhost:3000/urlunescape/https://uk.wikipedia.org/wiki/%u0423%u043A%u0440%u0430%u0457%u043D%u0430
@@ -36,24 +37,17 @@ app = FastAPI(
 # http://localhost:3000/unescape_url/https://uk.wikipedia.org/wiki/%u0423%u043A%u0440%u0430%u0457%u043D%u0430
 @app.get("/unescape_url/{url_to_unescape:path}")
 async def unescape_url(url_to_unescape: str = Query(alias='url', title='Tiiiiitle', description='Descriiiiption descriiiiption descriiiiption')) -> str:
-    if not url_to_unescape:
-        raise ValueError('')
-    url_to_unescape = url_to_unescape.strip('\n')
-    regex_patterns = {
-        'html_entity': r'[&]([a-z]+)|([#][0-9]+)[;]',
-        'escaped': r'%[uU]([0-9A-Fa-f]{4})',
-        'percent_encoded': r'%([0-9A-Fa-f]{2})'
-    }
-    how_many_passes = 3
-    for _ in range(how_many_passes):
-        if re.search(regex_patterns['html_entity'], url_to_unescape):
-            url_to_unescape = html.unescape(url_to_unescape)
-        if re.search(regex_patterns['escaped'], url_to_unescape):
-            url_to_unescape = re.sub(regex_patterns['escaped'], lambda m: chr(
-                int(m.group(1), 16)), url_to_unescape)
-        if re.search(regex_patterns['percent_encoded'], url_to_unescape):
-            url_to_unescape = urllib.parse.unquote(url_to_unescape)
-    return url_to_unescape
+    proc_url_unescape = subprocess.Popen(
+        ['/usr/bin/python', '../url_tools/url_unescape.py', url_to_unescape],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+    )
+    proc_url_unescape.stdin.write(url_to_unescape.encode())
+    proc_url_unescape.stdin.flush()
+    output_as_bytes, err_as_bytes = proc_url_unescape.communicate()
+    if err_as_bytes and not output_as_bytes:
+        print(err_as_bytes)
+    return output_as_bytes.decode()
 
 
 # http://localhost:3000/fix_legacy_encoding?str_to_fix=GocÅ‚awski&encoding_from=&encoding_to=&expected_str=Gocławski&recursivity_depth=
